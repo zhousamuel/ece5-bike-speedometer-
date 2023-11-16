378314306f1a854e377ae2294bb69bbd2defb8e6
SAMMY <szhou153@ucsb.edu>
2023-11-16 11:27:16 -0800

    adding starter file, print on LCD

---
+++ main/main.ino
@@ -0,0 +1,28 @@
+#include <LiquidCrystal.h>
+
+int led = 13; //LED Pin
+int sensor = 10; //sensor pin
+int val; //numeric variable
+int counter = 0; //count how many times magnet
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  pinMode(led, OUTPUT); //set led as output
+  pinMode(sensor, INPUT); //set sensor pin as input
+  lcd.begin(16, 2); //set up the LCD's number of columns and rows
+  lcd.print("BIKE SPEEDOMETER"); // print BIKE SPEEDOMETER on LCD
+}
+
+void loop() {
+  lcd.setCursor(0, 1);
+  val = digitalRead(sensor);
+  if(val == LOW)
+  {
+    counter += 1;
+  }
+  lcd.print("Counter: ");
+  lcd.print(counter);
+  delay(167);
+
+}
