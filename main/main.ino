f2660f474900c9dc9b95b750aff78f3eefc1d833
SAMMY <szhou153@ucsb.edu>
2023-11-16 17:27:58 -0800

    added contrast setting to remove use of resistors in circuit

--- main/main.ino
+++ main/main.ino
@@ -5,10 +5,14 @@ int sensor = 10; //sensor pin
 int val; //numeric variable
 float hall_thresh = 100.0;
 int counter = 0; //count how many times magnet
+int Contrast = 75;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
+
+  analogWrite(6, Contrast); // set contrast to pin 6
+
   pinMode(led, OUTPUT); //set led as output
   pinMode(sensor, INPUT); //set sensor pin as input
   lcd.begin(16, 2); //set up the LCD's number of columns and rows
@@ -24,6 +28,6 @@ void loop() {
   }
   lcd.print("Counter: ");
   lcd.print(counter);
-  delay(167);
 
+  delay(100);
 }
