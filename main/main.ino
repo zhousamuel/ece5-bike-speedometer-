7673a8a6293de70664d285ad1e4e7345e0e3c66e
SAMMY <szhou153@ucsb.edu>
2023-12-07 12:51:29 -0800

    working code

--- main/main.ino
+++ main/main.ino
@@ -1,23 +1,19 @@
 #include <LiquidCrystal.h>
+#include <Adafruit_NeoPixel.h>
 const float pi = 3.14;   
 // int led = 13 // LED Pin
 int sensor = 13;  //sensor pin
-int counter; //count how many times magnet
+float counter; //count how many times magnet
 //int Contrast = 75
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 float bike_radius = 0.28; //m
-const float timeInterval = 5000; // update speed every 5 seconds
+float bike_circum = 1.75912; // m
+const float timeInterval = 3000; // update speed every 3 seconds
 
 float distance = 0;
 int rotations = 0;
-
-// ISR(TIMER1_COMPA_vect)
-// {
-// count++;
-// flash();
-// }
 
 void setup() {
 
@@ -28,13 +24,6 @@ void setup() {
   pinMode(sensor, INPUT);         //set sensor pin as input (13)
   lcd.begin(16, 2);        //set up the LCD's number of columns and rows
 
-  // cli();
-  // TCCR1A = 0;
-  // TCCR1B = 0; 
-  // OCR1A = reload;
-  // TCCR1B = (1<<WGM12) | (1<<CS12); 
-  // TIMSK1 = (1<<OCIE1A); 
-  // sei(); 
 }
 
 void loop() {
@@ -47,7 +36,7 @@ void loop() {
 
   int previous = LOW;
 
-  while(millis() - event_started < 5000){
+  while(millis() - event_started < timeInterval){
 
     //ADD CODE FOR THE TURN LIGHTS
 
@@ -62,19 +51,16 @@ void loop() {
   }
 
   counter /= 2; // NEEDED FOR CALCULATIONS
-
   // speed -> (circumference * rpm) / time (milliseconds)
   // m * (counter * 12 rpm) / (time * 1000)
   // circumference : meters
   // counter : reveloutions per second
   // timeInterval = seconds
 
-  //meters * rev/5s * 1 mile/1609.344 meters * 5 * 60s/1min * 60min/1hr  
-  //miles/5 seconds 
-  //1609.344
-
   // float speed = (circumference * (counter / 5)) / (timeInterval / 1000); <- 
-  float speed =  (counter * 12 * (0.56 * pi)) / 1609.344;
+  //float speed =  (counter * 12 * (0.56 * pi)) / 1609.344;
+  float meters_per_5_seconds = counter * bike_circum;
+  float speed = (meters_per_5_seconds * 3600) / (1609.34 * 3);
   //lcd.print(counter);
   lcd.print(speed);
   lcd.print(" mph");
@@ -84,8 +70,8 @@ void loop() {
 
    lcd.setCursor(0, 1);
   // bike_radius => m
-  distance = (rotations) * (2*pi*bike_radius);
+   distance = (rotations) * (2*pi*bike_radius);
    lcd.print("Distance: ");
    lcd.print(rotations);
-   lcd.print("m");
+   lcd.print(" m");
 }
